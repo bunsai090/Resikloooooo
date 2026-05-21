7a2ba2a2da9240fb1d0ce46b8f6df6fd0903bb99
bunsai <bunsai090saporno@gmail.com>
2026-05-21 08:38:54 +0800

    fix: resolve react-map-gl/mapbox using absolute path in vite alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      'react-map-gl/mapbox': 'react-map-gl/dist/mapbox.js',
+      'react-map-gl/mapbox': path.resolve(__dirname, './node_modules/react-map-gl/dist/mapbox.js'),
     },
   },
   server: {
